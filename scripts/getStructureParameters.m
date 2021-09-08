049bdda175ae96411917f73720d4dfe8d28e6421
Roscoe <45630405+roscoelai@users.noreply.github.com>
2021-09-08 14:25:51 +0800

    Update getStructureParameters.m
    Reduce number of evaluations and improve readability.

--- scripts/getStructureParameters.m
+++ scripts/getStructureParameters.m
@@ -1,9 +1,9 @@
-function p = getStructureParameters(mystruct, myfield, value)
-% Sets p to mystruct.myfield if it exists, other assigns it to value
-if  ~exist('value', 'var') && ~isfield(mystruct, myfield)
-    error('Either value of mystruct.myfield must exist');
-elseif exist('value', 'var') && ~isfield(mystruct, myfield) 
-    p = value;
-else
+function p = getStructureParameters(mystruct, myfield, default_value)
+% Returns mystruct.myfield if it exists, or default_value otherwise
+if isfield(mystruct, myfield)
     p = mystruct.(myfield);
+elseif exist("default_value", "var")
+    p = default_value;
+else
+    error("Either 'mystruct.myfield' or 'default_value' must exist");
 end
